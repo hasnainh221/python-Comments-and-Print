ce1d5b9524950d8b069abc74796dc0f414eca274
Hasnain Haider <111046199+hasnainh221@users.noreply.github.com>
2024-07-06 06:06:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# this is my name
+print ("hasnain1")
+print ("this escape \"sequence is used for new line")
+print ("this escape sequence\t is used for tab")
+print ("hasnain2")
+print ("this escape sequence\n is used for new line")
+
+print ("hasnain3")
+print ("this escape sequence\n is used for new line")
